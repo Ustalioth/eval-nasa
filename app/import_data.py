18a94f2aeef3722a3260fda325aa5858eccf4c37
Matthieu FRANCOIS <matth.fra@gmail.com>
2023-09-15 14:27:56 +0200

    Import en DB ok

--- app/import_data.py
+++ app/import_data.py
@@ -1,7 +1,16 @@
 from pyspark.sql import SparkSession
 
+# Create a Spark session
+spark = SparkSession.builder \
+    .appName("CSV to MariaDB") \
+     .config("spark.jars", "./mysql-connector-j-8.1.0.jar") \
+     .getOrCreate()
+
+# Read CSV data
+csv_data = spark.read.option("header", "true").csv("star_classification.csv")
+
 # Configure the MariaDB JDBC connection
-jdbc_hostname = "localhost"
+jdbc_hostname = "mysql"
 jdbc_port = 3306
 jdbc_database = "star_classification"
 jdbc_username = "nasa"
@@ -9,19 +18,13 @@ jdbc_password = "verystrongpassword123*"
 
 jdbc_url = f"jdbc:mysql://{jdbc_hostname}:{jdbc_port}/{jdbc_database}"
 
-# Create a Spark session
-spark = SparkSession.builder.appName("CSV to MariaDB").config("spark.jars", "/app/mysql-connector-java-8.0.13.jar").getOrCreate()
-
-# Read CSV data
-csv_data = spark.read.option("header", "true").csv("/app/star_classification.csv")
-
 connection_properties = {
-    "user": jdbc_username,
-    "password": jdbc_password,
+        "user": jdbc_username,
+         "password": jdbc_password,
 }
 
 # Write the data to MariaDB
-csv_data.write.mode("overwrite").option("url", jdbc_url).jdbc(jdbc_url, "target_table_name", properties=connection_properties)
+csv_data.write.mode("overwrite").option("driver", "com.mysql.cj.jdbc.Driver").jdbc(jdbc_url, "target_table_name", properties=connection_properties)
 
 # Stop the Spark session
 spark.stop()
