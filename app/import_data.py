330aaaee8a263f197e3e1b2531dee49365a99fc9
Aude-ifa <59014470+Aude-ifa@users.noreply.github.com>
2023-09-15 15:37:54 +0200

    # Fix files

--- app/import_data.py
+++ app/import_data.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+import matplotlib.pyplot as plt
 
 # Create a Spark session
 spark = SparkSession.builder \
@@ -25,6 +26,41 @@ connection_properties = {
 
 # Write the data to MariaDB
 csv_data.write.mode("overwrite").option("driver", "com.mysql.cj.jdbc.Driver").jdbc(jdbc_url, "target_table_name", properties=connection_properties)
+
+# Lire les données depuis la base de données MariaDB
+db_data = spark.read.jdbc(url=jdbc_url, table="target_table_name", properties=connection_properties)
+
+# Afficher les données récupérées
+db_data.show()
+
+# Obtenir le décompte de chaque classe
+class_counts = db_data.groupBy("class").count().orderBy("class")
+
+class_names = class_counts.select("class").rdd.flatMap(lambda x: x).collect()
+class_values = class_counts.select("count").rdd.flatMap(lambda x: x).collect()
+
+colors = ["blue", "green", "red"]
+
+
+plt.figure(figsize=(10, 6))
+plt.bar(class_names, class_values, color=colors)
+plt.xlabel("Classes")
+plt.ylabel("Nombre d'occurrences")
+plt.title("Répartition des classes")
+plt.xticks(rotation=45)
+plt.savefig("/app/class_distribution.png")
+
+
+total_samples = db_data.count()
+class_proportions = [(count / total_samples) for count in class_values]
+
+plt.figure(figsize=(8, 8))
+plt.pie(class_proportions, labels=class_names, autopct='%1.1f%%', startangle=140)
+plt.axis('equal')
+plt.title("Proportion des classes")
+plt.savefig("/app/class_proportion.png")
+plt.show()
+
 
 # Stop the Spark session
 spark.stop()
